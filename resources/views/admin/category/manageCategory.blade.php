ab9c541713e37c5be42aabbce0cae10a65d10df4
unknown <you@gmail git config --global user.email you@gmail.com.com>
2019-02-21 17:19:35 +0600

    ajax using category modal

--- resources/views/admin/category/manageCategory.blade.php
+++ resources/views/admin/category/manageCategory.blade.php
@@ -22,7 +22,10 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <?php $i = 1; ?>
+                            <?php
+                            $i = 1;
+                            $index = 0;
+                            ?>
                             @foreach($categories as $category)
                             <tr>
                                 <td>{{$i++}}</td>
@@ -36,8 +39,12 @@
                                     <a href="{{url('/category/delete/'.$category->id)}}" class='btn btn-danger' onclick="return confirm('Are you sure want to delete?')">
                                         <span class="glyphicon glyphicon-trash"></span>
                                     </a>
+                                    <a  class="btn btn-success" onclick="get_category_info_data('<?php echo $index; ?>')" href="javascript:void(0)">Edit</a>                  
                                 </td>
                             </tr>
+                            <?php
+                            $index++;
+                            ?>
                             @endforeach
                         </tbody>
                     </table>
@@ -49,6 +56,48 @@
         <!-- /.panel -->
     </div> 
 </div>
+<div class="modal fade" id="noteModal" tabindex="-1" role="dialog" aria-labelledby="paymentModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title text-center">Edit Category Information</h4>
+            </div>
+            <div class="modal-body">
+                <div class="panel-body">
+                    <form class="form-horizontal" id="update_user_note" action="javascript:void(0);">
+                        <input type="hidden" class="form-control" id="id">
+                        <div class="form-group">
+                            <label for="inputEmail3" class="col-sm-4 control-label">Name</label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control" id="name" placeholder="Enter name" required="">
+                            </div>
+                        </div>                        
+                    </form>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-success" onclick="">Update</button>
+            </div>
+        </div>
+    </div>
+</div>
 
+<script type="text/javascript">
+    localStorage.setItem('category_info', '<?php echo json_encode($categories) ?>');
+
+    function get_category_info_data(index) {
+
+        var category_info = JSON.parse(localStorage.getItem('category_info'));
+
+        $('#noteModal').modal();
+
+        jQuery('input[id=id]').val(category_info[index].id);
+        jQuery('input[id=name]').val(category_info[index].categoryName);
+
+        console.log(category_info[index]);
+
+    }
+</script>
 
 @endsection
